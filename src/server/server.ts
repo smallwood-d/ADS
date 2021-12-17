179127ae0d8c4320d9c121765a6560c71b3aaeb3
SmallWood-D <shay.gozan@gmail.com>
2021-12-17 20:46:16 +0200

    send to all interfaces

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,10 @@
 import dgram, { Socket } from 'dgram';
 import { Buffer } from 'buffer';
 
+import { getInterfaces } from '../utils';
 import cfg from '../resources/cfg.json';
 
+const ips = getInterfaces();
 const server: Socket = dgram.createSocket("udp4"); 
 
 server.bind(cfg.S_PORT, () => {
@@ -13,7 +15,8 @@ setInterval(broadcastNew, 3000);
 
 function broadcastNew() {
     const message = Buffer.from('Some bytes');
-    server.send(message, cfg.C_PORT,(err: any) => {
+    const sendMsg = (ip: string) => {
+        server.send(message, cfg.C_PORT, ip, (err: any) => {
         if (err) {
             console.log(`Error OMNG ${err}`);
             console.log(`Closing the socket`);
@@ -21,6 +24,8 @@ function broadcastNew() {
         }
         console.log(`send msg to clinets...`);               
       });
+    }
+    ips.forEach((ip) => ip && sendMsg(ip));
     console.log("Sent " + message + " to the wire...");
 }
 
